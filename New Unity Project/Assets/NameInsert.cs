5bc483bb868188b9e37e4210a1a8a9b0d32923a4
Alessio Acampora <acampora199@gmail.com>
2021-10-05 19:02:46 +0100

    Setup Fungus + name feature

---
+++ New Unity Project/Assets/NameInsert.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class NameInsert : MonoBehaviour
+{
+    public Flowchart fc;
+
+    public void SaveName(string name)
+    {
+        fc.SetStringVariable("player", name);
+        fc.SetBooleanVariable("nameEntered", true);
+    }
+}
